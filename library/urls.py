be0e694e702a5278f7dbabd17b50d34d3563df19
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-12-09 14:13:04 +0300

    first commit

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import BookListView, BookCreateView, BookDeleteView, BookUpdateView, BookDetailView
 
 app_name = 'library'
 
 urlpatterns = [
-    path('books_list/', views.books_list, name='books_list'),
-    path('books_detail/<int:book_id>', views.book_detail, name='books_detail')
+    path('books/', BookListView.as_view(), name='books_list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/new/', BookCreateView.as_view(), name='book_create'),
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book_edit'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
 from .models import Book
 
-def books_list(request):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'library/books_list.html', context)
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'library/books_list.html'
+    context_object_name = 'books'
 
 
-def book_detail(request, book_id):
-    book = Book.objects.get(id=book_id)
-    context = {'book': book}
-    return render(request, 'library/books_detail.html', context)
+class BookCreateView(CreateView):
+    model = Book
+    fields = ['title', 'publication_date', 'author']
+    template_name = 'library/book_form.html'
+    success_url = reverse_lazy('library:books_list')
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'library/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ['title', 'publication_date', 'author']
+    template_name = 'library/book_form.html'
+    success_url = reverse_lazy('library:books_list')
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'library/book_confirm_delete.html'
+    success_url = reverse_lazy('library:books_list')
+
+
+# def books_list(request):
+#     books = Book.objects.all()
+#     context = {'books': books}
+#     return render(request, 'library/books_list.html', context)
+#
+#
+# def book_detail(request, book_id):
+#     book = Book.objects.get(id=book_id)
+#     context = {'book': book}
+#     return render(request, 'library/book_detail.html', context)

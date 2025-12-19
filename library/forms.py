c8e5117f6c74e60d025784b4b5df38e384e6b716
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-12-19 16:34:16 +0300

    first commit

--- library/forms.py
+++ library/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Author, Book
 
 class AuthorForm(forms.ModelForm):
@@ -6,8 +8,54 @@ class AuthorForm(forms.ModelForm):
         model = Author
         fields = ["first_name", "last_name", "birth_date"]
 
+    def __init__(self, *args, **kwargs):
+        super(AuthorForm, self).__init__(*args, **kwargs)
+
+        self.fields["first_name"].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите имя'
+        })
+
+        self.fields["last_name"].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите фамилию'
+        })
+
+        self.fields["birth_date"].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите дату'
+        })
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
+
+        if Author.objects.filter(first_name=first_name, last_name=last_name):
+            raise ValidationError("Автор с таким именем и фамилией уже существует")
+        return cleaned_data
+
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ["title", "publication_date", "author"]
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название'
+        })
+
+        self.fields["publication_date"].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите дату публикации'
+        })
+
+        self.fields["author"].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите автора'
+        })
+

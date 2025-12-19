d10436750adce86a4f12f75bf527924dbe1655fd
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-12-19 14:40:31 +0300

    first commit

--- library/urls.py
+++ library/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 # from . import views
-from .views import BookListView, BookCreateView, BookDeleteView, BookUpdateView, BookDetailView
+from .views import BookListView, BookCreateView, BookDeleteView, BookUpdateView, BookDetailView, AuthorCreateView, \
+    AuthorUpdateView, AuthorListView
 
 app_name = 'library'
 
 urlpatterns = [
+    path('authors/', AuthorListView.as_view(), name='author_list'),
+    path('author/new/', AuthorCreateView.as_view(), name='author_create'),
+    path('author/update/<int:pk>/', AuthorUpdateView.as_view(), name='author_update'),
     path('books/', BookListView.as_view(), name='books_list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('books/new/', BookCreateView.as_view(), name='book_create'),
--- library/views.py
+++ library/views.py
@@ -1,20 +1,50 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from .models import Book
+from .forms import BookForm, AuthorForm
+from .models import Book, Author
 
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'library/author_list.html'
+    context_object_name = 'authors'
+
+
+
+class AuthorCreateView(CreateView):
+    model = Author
+    form_class = AuthorForm
+    template_name = 'library/author_form.html'
+    success_url = reverse_lazy('library:author_list')
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    form_class = AuthorForm
+    template_name = 'library/author_form.html'
+    success_url = reverse_lazy('library:author_list')
+
+
+
+
 
 class BookListView(ListView):
     model = Book
     template_name = 'library/books_list.html'
     context_object_name = 'books'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(publication_date__year__gt=2000)
+
 
 class BookCreateView(CreateView):
     model = Book
-    fields = ['title', 'publication_date', 'author']
+    form_class = BookForm
     template_name = 'library/book_form.html'
     success_url = reverse_lazy('library:books_list')
 
@@ -24,12 +54,18 @@ class BookDetailView(DetailView):
     template_name = 'library/book_detail.html'
     context_object_name = 'book'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author_books_count'] = Book.objects.filter(author=self.object.author).count()
+        return context
+
 
 class BookUpdateView(UpdateView):
     model = Book
-    fields = ['title', 'publication_date', 'author']
+    form_class = BookForm
     template_name = 'library/book_form.html'
     success_url = reverse_lazy('library:books_list')
+
 
 class BookDeleteView(DeleteView):
     model = Book
